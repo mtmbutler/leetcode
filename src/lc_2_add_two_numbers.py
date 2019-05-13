583454d3a84e8feb7c6b6632939f4e5fe586aa98
Miles Butler <mtmbutler@icloud.com>
2019-05-13 10:14:05 -0500

    Add test cases and modify #2

--- src/lc_2_add_two_numbers.py
+++ src/lc_2_add_two_numbers.py
@@ -1,5 +1,5 @@
 """
-https://leetcode.com/problems/add-two-numbers/description/
+leetcode.com/problems/add-two-numbers
 
 You are given two non-empty linked lists representing two non-negative
 integers. The digits are stored in reverse order and each of their nodes
@@ -22,6 +22,39 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+        
+    
+class LinkedList:
+    """A helper class for running test cases."""
+    def __init__(self, arg):
+        """arg can be a list or just the first node."""
+        if isinstance(arg, ListNode) or isinstance(arg, LinkedList):
+            self.val = arg.val
+            self.next = arg.next
+        elif isinstance(arg, list):
+            first_node = ListNode(arg[0])
+            nodes = [first_node]
+            for i, v in enumerate(arg[1:]):
+                n = ListNode(v)
+                nodes[i].next = n
+                nodes.append(n)
+
+            # Set self to the first node
+            self.val = first_node.val
+            self.next = first_node.next
+        else:
+            raise TypeError("arg must be list or ListNode.")
+
+    def __eq__(self, other):
+        return self.to_list() == other.to_list()
+
+    def to_list(self):
+        node = self
+        li = [node.val]
+        while node.next is not None:
+            node = node.next
+            li.append(node.val)
+        return li
 
 
 def ll_to_list(node):
@@ -51,23 +84,20 @@ class Solution:
         li_1 = ll_to_list(l1)
         li_2 = ll_to_list(l2)
 
+        # Reverse and add
         num = int(''.join(li_1[::-1])) + int(''.join(li_2[::-1]))
-        print(num)
-        li = [ch for ch in str(num)]
-        print(li)
-        li = li[::-1]
-        print(li)
-        return list_to_ll(li)
 
-# def main():
-#     """Summary
-#     """
-#     sol = Solution()
-#     test_cases = [test_one, test_two]
-#     for case in test_cases:
-#         print('Input: {}\nOutput: {}\n------------'
-#               .format(case, sol.methodName(case)))
+        # Split back into linked list
+        return list_to_ll([int(i) for i in list(str(num))[::-1]])
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    sol = Solution()
+    test_cases = (
+        ([LinkedList([2, 4, 3]), LinkedList([5, 6, 4])],
+         LinkedList([7, 0, 8])),
+        ([LinkedList([4, 3]), LinkedList([5, 6, 4])],
+         LinkedList([9, 9, 4])),
+    )
+    for args, out in test_cases:
+        assert LinkedList(sol.addTwoNumbers(*args)) == out
